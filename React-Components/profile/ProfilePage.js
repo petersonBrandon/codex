8c843d6bb151e26bbde76433487ef1a2046826a1
Brandon Peterson <brandonacfuntech@gmail.com>
2022-02-26 17:32:37 -0600

    Adjusted Window navigation and styling.

--- React-Components/profile/ProfilePage.js
+++ React-Components/profile/ProfilePage.js
@@ -1,18 +1,23 @@
 import styles from '../../styles/profile/profile.module.css';
 import Projects from './Projects';
 import Settings from './Settings';
+import {useState} from 'react'
 
 const ProfilePage = () => {
+    const PROFILE = "profile";
+    const PROJECTS = "projects";
+    const [window, setWindow] = useState(PROFILE);
+
     return (
         <div className={styles.profileContainer}>
             <section className={styles.sideNav}>
-                <div className={styles.navBtn}>Profile</div>
-                <div className={styles.navBtn}>Projects</div>
+                <div className={styles.navBtn} onClick={() => setWindow(PROFILE)}>Profile</div>
+                <div className={styles.navBtn} onClick={() => setWindow(PROJECTS)}>Projects</div>
             </section>
 
             <section className={styles.content}>
-                <Settings />
-                <Projects />
+                {window === PROFILE ? <Settings /> : <meta />}
+                {window === PROJECTS ? <Projects /> : <meta />}
             </section>
         </div>
     )
